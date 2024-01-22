fc53939a35cf10481b8d8e875accd55cb7035eeb
Ruslan Dovhanych <imdakris@gmail.com>
2024-01-22 11:27:29 +0200

    add method .pack

--- window.py
+++ window.py
@@ -12,7 +12,7 @@ class Window:
         title="MyWindow",
         resizable=(False, False),
         icon=None,
-    ) -> None:
+    ):
         self.root = Tk()
         self.root.title(title)
         self.root.geometry(f"{width}x{height}+200+200")
@@ -20,9 +20,22 @@ class Window:
         if icon:
             self.root.iconbitmap(icon)
 
+        self.label = Label(
+            self.root,
+            text="I'm label",
+            bg="#4efc03",
+            relief="ridge",
+            wraplength=50,
+            font="Consolas 15",
+        )
+
     def run(self):
         """Launches a window"""
+        self.draw_widgets()
         self.root.mainloop()
+
+    def draw_widgets(self):
+        self.label.pack(anchor=NW, padx=20, pady=70)
 
     def create_child(
         self, width, height, title="Child", resizable=(False, False), icon=None
@@ -32,6 +45,6 @@ class Window:
 
 if __name__ == "__main__":
     window = Window(500, 500, "Tkinter")
-    window.create_child(200, 100)
+    # window.create_child(200, 100)
 
     window.run()
